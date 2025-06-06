182d4249d8355fa0ec4bb3cc3ec2b767abf1250c
zerorebirth <107281920+zerorebirth@users.noreply.github.com>
2025-06-06 18:19:48 -0400

    Update test_LevelsFYIScraper.py
    Added spaces ("\n") for more clarity when reading.

--- Parser Files/test_LevelsFYIScraper.py
+++ Parser Files/test_LevelsFYIScraper.py
@@ -11,19 +11,27 @@
 #Input variables:
 
 #s: URL of first page of companies that user wants to scrape
+
 #parser: type of parser that user wants to use (ex: html.parser, html5lib, etc.)
+
 #offset: used to determine next page of companies that should be recorded
 #(check levels.fyi url on page 2 of your search to see this)
+
 #max_offset: the very last page's offset + 1
+
 #offset_increment: the increment between page offsets
 #(third page offset - second page offset, if only two pages are searched,
 #then any integer works)
+
 #s2: the first half of the url for the companies starting from page 2,
 #determined by taking offsets and merging with url
+
 #s3: the second half of the url for the companies starting from page 2,
 #determined by taking offsets and merging with url 
+
 #continue_program: boolean (true or false) that stops program after 3 attempts if false,
 #continues otherwise
+
 #stack: companies the user wants to record are added to this stack
 
 #Output variable:
@@ -33,17 +41,27 @@
 #Instructions:
 
 #All arguments except the offsets and continue_program must be python strings.
+
 #Offsets and continue_program must be integers.
+
 #When running in terminal, after typing in the name of the .py file, enter in:
 
 #The first url to be scraped,
+
 #parser type,
+
 #offset from second url,
+
 #offset from the very last url + 1,
+
 #offset increment (third url offset - second url offset,
+
 #if only two pages then any integer works),
+
 #the second page url up to and including 'offset=',
+
 #the rest of the second page url,
+
 #and 0 if you only want to attempt the program three times (1 otherwise).
 
 #Refer to the reference notebook in case you are confused.
